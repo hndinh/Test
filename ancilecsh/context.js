1a0be2623cf4ae4fa1a3ef79d5717d65144cfddd
Hung Dinh <dnhung81@yahoo.com>
2017-04-05 09:15:40 -0400

    Testing new type

--- ancilecsh/context.js
+++ ancilecsh/context.js
@@ -55,7 +55,7 @@ ANCILECSH.Context = ANCILECSH.Context || new (function(private, public) {
 			{
 				var view = sap.ui.getCore().byId(allDivs[i].id);
 				if(view !== undefined && view !== null){
-					if(view instanceof sap.ui.core.mvc.View){
+					if(view instanceof sap.ui.core.controls.View){
 						allViews.push(view);
 					}
 				}
